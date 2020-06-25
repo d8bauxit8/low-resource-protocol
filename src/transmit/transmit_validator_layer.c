b7da3ff94fe43799b24d6b85190c532b100f43e6
D8bauxit8 <mark.jehoda@gmail.com>
2020-06-25 18:49:23 +0200

    Bugfix - the validation layer have to step the validatorCurrentFrame's pointer to the next frame

--- src/transmit/transmit_validator_layer.c
+++ src/transmit/transmit_validator_layer.c
@@ -14,4 +14,5 @@ void LRP_TransmitValidatorLayer_handler(_LRPSessionProvider *const sessionProvid
     LRP_Frame_addDataToFrameBufferFromFrameData(sessionProvider->validatorCurrentFrame);
 
     LRP_Frame_setStatus(sessionProvider->validatorCurrentFrame, TRANSMIT_FRAME_READY_TO_TRANSMIT);
+    sessionProvider->validatorCurrentFrame = sessionProvider->validatorCurrentFrame->next;
 }
