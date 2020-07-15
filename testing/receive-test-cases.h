ae073d73db9dcb1626422ec9394bf50a7cfaf7cb
D8bauxit8 <mark.jehoda@gmail.com>
2020-07-15 15:44:55 +0200

    Remove old test cases

--- testing/receive-test-cases.h
+++
@@ -1,8 +0,0 @@
-#ifndef RECEIVE_TEST_CASES_H
-#define RECEIVE_TEST_CASES_H
-
-#include "receive-testing.h"
-
-void test_receive(void);
-
-#endif // RECEIVE_TEST_CASES_H
--- testing/transmit-test-cases.h
+++
@@ -1,8 +0,0 @@
-#ifndef TRANSMIT_TEST_CASES_H
-#define TRANSMIT_TEST_CASES_H
-
-#include "transmit-testing.h"
-
-void test_transmit(void);
-
-#endif // TRANSMIT_TEST_CASES_H
