31ca4f5f15816f2093d78840f6524906511b947e
d8bauxit8 <mark.jehoda@gmail.com>
2023-01-29 23:31:10 +0100

    fix: XC8 will compile function pointer array right

--- src/receive/receive-application-layer.c
+++ src/receive/receive-application-layer.c
@@ -4,7 +4,7 @@
  * Public method declarations
  */
 void LRP_ReceiveApplicationLayer_handler(LRPReceiveSessionProvider *const sessionProvider,
-                                         const LRPReceiveFrameController receiveFrameControllerList[],
+                                         const LRPReceiveFrameController *const receiveFrameControllerList,
                                          const unsigned char numberOfReceiveFrameControllers) {
     if (sessionProvider->applicationCurrentFrame->status != RECEIVE_FRAME_READY_TO_READ) {
         return;
@@ -12,8 +12,7 @@ void LRP_ReceiveApplicationLayer_handler(LRPReceiveSessionProvider *const sessio
 
     unsigned char i = 0;
     for (; i < numberOfReceiveFrameControllers; i++) {
-        LRPReceiveFrameController controller = *receiveFrameControllerList[i];
-        if (controller((const LRPFrameData *const) sessionProvider->applicationCurrentFrame)) {
+        if ((*receiveFrameControllerList[i])((const LRPFrameData *const) sessionProvider->applicationCurrentFrame)) {
             break;
         }
     }
